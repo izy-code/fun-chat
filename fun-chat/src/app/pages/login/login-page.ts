607abaeb3650864409568d9c0f62c9c10bd2504b
Izy <126877709+izy-code@users.noreply.github.com>
2024-04-13 13:00:08 +0300

    feat: add login page

--- fun-chat/src/app/pages/login/login-page.ts
+++ fun-chat/src/app/pages/login/login-page.ts
@@ -1,137 +1,175 @@
 import './login-page.scss';
-import BaseComponent from '@/app/components/base-component';
-import { form, h1, main } from '@/app/components/tags';
+import type BaseComponent from '@/app/components/base-component';
+import { a, form, h1, main } from '@/app/components/tags';
 import ButtonComponent from '@/app/components/button/button';
 import LoginFieldComponent from './login-field/login-field';
 import type LocalStorage from '@/app/utils/session-storage';
 import type Router from '@/app/router/router';
 import Pages from '@/app/router/pages';
+import PageComponent from '@/app/components/page-component';
 
 enum FieldMinLength {
-  NAME = 3,
-  SURNAME = 4,
+  LOGIN = 3,
+  PASSWORD = 6,
 }
 
-export default class LoginPageComponent extends BaseComponent {
-  private router: Router;
+type ValidationSwitch = Partial<{
+  CHAR_SET: boolean;
+  LENGTH: boolean;
+  CASE: boolean;
+}>;
 
-  private storage: LocalStorage;
+const LOGIN_VAL_SWITCH = {
+  LENGTH: true,
+};
 
-  private submitButton: BaseComponent<HTMLButtonElement>;
+const PASSWORD_VAL_SWITCH = {
+  LENGTH: true,
+  CASE: true,
+  CHAR_SET: true,
+};
 
-  private nameField: LoginFieldComponent;
+export default class LoginPageComponent extends PageComponent {
+  private loginButton: BaseComponent<HTMLButtonElement>;
 
-  private surnameField: LoginFieldComponent;
+  private loginField: LoginFieldComponent;
+
+  private passwordField: LoginFieldComponent;
 
   constructor(router: Router, storage: LocalStorage) {
-    super({ className: 'app-container__page login-page' });
+    super(router, storage);
 
-    this.router = router;
-    this.storage = storage;
+    this.addClass('login-page');
 
-    this.nameField = new LoginFieldComponent('First Name:');
-    this.nameField.addClass('login-page__field');
-    this.nameField.setInputAttribute('autofocus', '');
-    this.nameField.addListener('input', this.onFieldInput.bind(this));
+    this.loginField = new LoginFieldComponent('Login:');
+    this.loginField.addClass('login-page__field');
+    this.loginField.setInputAttribute('autofocus', '');
+    this.loginField.addListener('input', this.onFieldInput.bind(this));
 
-    this.surnameField = new LoginFieldComponent('Surname:');
-    this.surnameField.addClass('login-page__field');
-    this.surnameField.addListener('input', this.onFieldInput.bind(this));
+    this.passwordField = new LoginFieldComponent('Password:');
+    this.passwordField.addClass('login-page__field');
+    this.passwordField.addListener('input', this.onFieldInput.bind(this));
 
-    this.submitButton = ButtonComponent({
+    this.loginButton = ButtonComponent({
       className: 'login-page__submit-button button',
       textContent: 'Login',
       buttonType: 'submit',
-      clickHandler: this.onSubmitButtonClick,
+      clickHandler: this.onLoginButtonClick,
     });
-    this.submitButton.setAttribute('disabled', '');
+    this.loginButton.setAttribute('disabled', '');
 
-    const headerComponent = h1('visually-hidden', 'Word puzzle application');
+    const aboutLink = a({
+      className: 'login-page__about-link button button--continue',
+      href: `#${Pages.ABOUT}`,
+      textContent: `About`,
+    });
+
+    const headingComponent = h1('visually-hidden', 'Fun chat application');
     const formComponent = form(
       { className: 'login-page__form', method: 'post' },
-      this.nameField,
-      this.surnameField,
-      this.submitButton,
+      this.loginField,
+      this.passwordField,
+      this.loginButton,
+      aboutLink,
     );
-    const mainComponent = main({ className: 'login-page__main' }, headerComponent, formComponent);
+    const mainComponent = main({ className: 'login-page__main' }, headingComponent, formComponent);
 
-    this.appendChildren([mainComponent]);
+    this.append(mainComponent);
   }
 
   private onFieldInput(evt: Event): void {
-    const nameValue: string = this.nameField.getInputValue();
-    const surnameValue: string = this.surnameField.getInputValue();
-
-    if (nameValue && surnameValue) {
-      this.submitButton.removeAttribute('disabled');
+    if (this.isLoginValid() && this.isPasswordValid()) {
+      this.loginButton.removeAttribute('disabled');
     } else {
-      this.submitButton.setAttribute('disabled', '');
+      this.loginButton.setAttribute('disabled', '');
     }
 
-    if (this.nameField.getNode() === evt.currentTarget) {
-      this.nameField.setErrorText('');
-      this.nameField.removeClass('login-field--error');
+    if (this.loginField.getNode() === evt.currentTarget) {
+      this.updateLoginErrorState();
     } else {
-      this.surnameField.setErrorText('');
-      this.surnameField.removeClass('login-field--error');
+      this.updatePasswordErrorState();
     }
   }
 
-  private static getFieldValidationErrors(fieldName: string, fieldValue: string, minLength: number): string {
-    const validCharsRegex = /^[a-zA-Z-]*$/;
+  private static getFieldValidationErrors(
+    fieldName: string,
+    fieldValue: string,
+    minLength: number,
+    valSwitch: ValidationSwitch,
+  ): string {
+    const { CHAR_SET = false, LENGTH = false, CASE = false } = valSwitch;
     let validationErrorsString = '';
 
-    if (!validCharsRegex.test(fieldValue)) {
-      validationErrorsString += `${fieldName} must contain only English letters and hyphens`;
+    if (CHAR_SET && !/\d/.test(fieldValue)) {
+      validationErrorsString += `${fieldName} must contain at least one digit`;
     }
 
-    if (fieldValue[0] !== fieldValue[0]?.toUpperCase()) {
-      validationErrorsString += `${fieldName} must start with an uppercase letter`;
+    if (CASE && (fieldValue.toUpperCase() === fieldValue || fieldValue.toLowerCase() === fieldValue)) {
+      validationErrorsString += `${fieldName} must contain both upper and lower case letters`;
     }
 
-    if (fieldValue.length < minLength) {
+    if (LENGTH && fieldValue.length < minLength) {
       validationErrorsString += `${fieldName} must be at least ${minLength} characters long`;
     }
 
     return validationErrorsString.split(fieldName).join(`\n⦁  ${fieldName}`).trim();
   }
 
-  private static updateFieldErrorState(field: LoginFieldComponent, errors: string): void {
-    if (errors) {
-      field.setErrorText(errors);
+  private static updateFieldErrorState(
+    field: LoginFieldComponent,
+    fieldName: string,
+    minLength: number,
+    valSwitch: ValidationSwitch,
+  ): void {
+    const fieldValue: string = field.getInputValue();
+    const fieldErrors = LoginPageComponent.getFieldValidationErrors(fieldName, fieldValue, minLength, valSwitch);
+    field.setErrorText(fieldErrors);
+
+    if (fieldErrors) {
       field.addClass('login-field--error');
+    } else {
+      field.removeClass('login-field--error');
     }
   }
 
-  private areFieldsValid(): boolean {
-    const nameFieldValue: string = this.nameField.getInputValue();
-    const surnameFieldValue: string = this.surnameField.getInputValue();
+  private static isFieldValid(field: LoginFieldComponent, minLength: number, valSwitch: ValidationSwitch): boolean {
+    const fieldValue: string = field.getInputValue();
+    const fieldErrors = LoginPageComponent.getFieldValidationErrors('', fieldValue, minLength, valSwitch);
 
-    const nameFieldErrors = LoginPageComponent.getFieldValidationErrors('Login', nameFieldValue, FieldMinLength.NAME);
-    const surnameFieldErrors = LoginPageComponent.getFieldValidationErrors(
-      'Password',
-      surnameFieldValue,
-      FieldMinLength.SURNAME,
-    );
-
-    LoginPageComponent.updateFieldErrorState(this.nameField, nameFieldErrors);
-    LoginPageComponent.updateFieldErrorState(this.surnameField, surnameFieldErrors);
-
-    return !nameFieldErrors && !surnameFieldErrors;
+    return !fieldErrors;
   }
 
-  private onSubmitButtonClick = (evt?: Event | undefined): void => {
+  private isLoginValid(): boolean {
+    return LoginPageComponent.isFieldValid(this.loginField, FieldMinLength.LOGIN, LOGIN_VAL_SWITCH);
+  }
+
+  private isPasswordValid(): boolean {
+    return LoginPageComponent.isFieldValid(this.passwordField, FieldMinLength.PASSWORD, PASSWORD_VAL_SWITCH);
+  }
+
+  private updateLoginErrorState(): void {
+    LoginPageComponent.updateFieldErrorState(this.loginField, 'Login', FieldMinLength.LOGIN, LOGIN_VAL_SWITCH);
+  }
+
+  private updatePasswordErrorState(): void {
+    LoginPageComponent.updateFieldErrorState(
+      this.passwordField,
+      'Password',
+      FieldMinLength.PASSWORD,
+      PASSWORD_VAL_SWITCH,
+    );
+  }
+
+  private onLoginButtonClick = (evt?: Event | undefined): void => {
     if (evt) {
       evt.preventDefault();
     }
 
-    if (this.areFieldsValid()) {
-      this.storage.setAuthData({
-        login: this.nameField.getInputValue(),
-        password: this.surnameField.getInputValue(),
-      });
+    this.storage.setAuthData({
+      login: this.loginField.getInputValue(),
+      password: this.passwordField.getInputValue(),
+    });
 
-      this.router.navigate(Pages.CHAT);
-    }
+    this.router.navigate(Pages.CHAT);
   };
 }
