d340162bfd574f8e33d21b2ff797fb4501781240
Izy <126877709+izy-code@users.noreply.github.com>
2024-04-19 14:35:40 +0300

    feat: add custom event names

---
+++ fun-chat/src/app/custom-events/index.ts
@@ -0,0 +1,8 @@
+enum CustomEventName {
+  SOCKET_STATE_CHANGE = 'socketStateChange',
+  SOCKET_MSG_SENT = 'socketMsgSent',
+  SOCKET_MSG_RECEIVED = 'socketMsgReceived',
+  MODAL_ERROR = 'modalError',
+}
+
+export default CustomEventName;
