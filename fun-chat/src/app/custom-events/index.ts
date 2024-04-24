b0b7475354d18a184dfa701e4fb34451076cbbbb
Izy <126877709+izy-code@users.noreply.github.com>
2024-04-24 21:53:09 +0300

    feat: add custom event names

--- fun-chat/src/app/custom-events/index.ts
+++ fun-chat/src/app/custom-events/index.ts
@@ -4,9 +4,16 @@ enum CustomEventName {
   SOCKET_MSG_RECEIVED = 'socketMsgReceived',
   MODAL_ERROR = 'modalError',
   CONTACTS_UPDATED = 'contactsUpdated',
+  MESSAGE_SENT = 'messageSent',
+  MESSAGE_RECEIVED = 'messageReceived',
 
   CONTACT_SELECTION_CLICK = 'contactSelectionClick',
   SEND_BTN_CLICK = 'sendBtnClick',
+  SEND_EDITED_BTN_CLICK = 'sendEditedBtnClick',
+  DIALOGUE_CLICK = 'dialogueClick',
+  USER_SCROLL = 'userScroll',
+  EDIT_BTN_CLICK = 'editBtnClick',
+  DELETE_BTN_CLICK = 'deleteBtnClick',
 
   SELECTED_LOGIN_CHANGED = 'selectedLoginChanged',
   SELECTED_ACTIVITY_CHANGED = 'selectedActivityChanged',
