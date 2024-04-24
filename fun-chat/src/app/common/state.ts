c5ec228b1372f8f59c61d88c55c6a4835e529ba4
Izy <126877709+izy-code@users.noreply.github.com>
2024-04-24 21:48:52 +0300

    feat: add deleteMessage and updateMessagesOnRead funcs, reorder methods

--- fun-chat/src/app/common/state.ts
+++ fun-chat/src/app/common/state.ts
@@ -5,69 +5,61 @@ import EventEmitter from './event-emitter';
 import SessionStorage from './session-storage';
 
 export default class State {
-  private static isSocketOpened = false;
-
-  private static selectedContact: string | null = null;
+  private static selectedContactLogin: string | null = null;
 
   private static isSelectedContactOnline = false;
 
   private static contactsData = new Map<string, ContactData>();
 
-  public static getContactsData = (): Map<string, ContactData> => State.contactsData;
+  public static getSelectedContactLogin = (): string | null => State.selectedContactLogin;
+
+  public static getSelectedContactActivity = (): boolean => State.isSelectedContactOnline;
+
+  public static setSelectedContactActivity = (isOnline: boolean): void => {
+    State.isSelectedContactOnline = isOnline;
+    EventEmitter.emit(CustomEventName.SELECTED_ACTIVITY_CHANGED, isOnline);
+  };
 
   public static getSelectedContactData = (): ContactData | null => {
-    if (State.selectedContact) {
-      return State.contactsData.get(State.selectedContact) || null;
+    if (State.selectedContactLogin) {
+      return State.contactsData.get(State.selectedContactLogin) || null;
     }
 
     return null;
   };
 
-  public static getSelectedContactLogin = (): string | null => State.selectedContact;
-
-  public static setSelectedContactActivity = (isOnline: boolean): void => {
-    this.isSelectedContactOnline = isOnline;
-    EventEmitter.emit(CustomEventName.SELECTED_ACTIVITY_CHANGED, isOnline);
-  };
-
-  public static getSelectedContactActivity = (): boolean => State.isSelectedContactOnline;
-
-  public static changeSocketState = (isOpened: boolean): void => {
-    State.isSocketOpened = isOpened;
-    State.clear();
-    EventEmitter.emit(CustomEventName.SOCKET_STATE_CHANGE, isOpened);
-  };
-
   public static setContactData = (login: string, data: ContactData): void => {
     if (!State.contactsData.has(login)) {
-      State.contactsData.set(login, data);
+      State.contactsData.set(login, { ...data, unreadMessagesCount: 0, messages: [] });
     } else {
       State.contactsData.set(login, { ...State.contactsData.get(login), ...data });
     }
   };
 
-  public static clear = (): void => {
-    State.contactsData.clear();
-    State.selectedContact = null;
-    State.isSelectedContactOnline = false;
-  };
+  public static getContactsData = (): Map<string, ContactData> => State.contactsData;
 
   public static setSelectedContact = (login: string | null): void => {
-    const startingState = State.selectedContact;
+    const startingState = State.selectedContactLogin;
 
-    if (State.selectedContact && !State.contactsData.has(State.selectedContact)) {
-      State.selectedContact = null;
+    if (State.selectedContactLogin && !State.contactsData.has(State.selectedContactLogin)) {
+      State.selectedContactLogin = null;
     } else {
-      State.selectedContact = login;
+      State.selectedContactLogin = login;
     }
 
-    if (State.selectedContact !== startingState) {
-      EventEmitter.emit(CustomEventName.SELECTED_LOGIN_CHANGED, State.selectedContact);
+    if (State.selectedContactLogin !== startingState) {
+      EventEmitter.emit(CustomEventName.SELECTED_LOGIN_CHANGED, State.selectedContactLogin);
 
-      if (State.selectedContact) {
-        State.setSelectedContactActivity(State.contactsData.get(State.selectedContact)!.isOnline);
+      if (State.selectedContactLogin) {
+        State.setSelectedContactActivity(State.contactsData.get(State.selectedContactLogin)!.isOnline);
       }
     }
+  };
+
+  public static clear = (): void => {
+    State.contactsData.clear();
+    State.setSelectedContact(null);
+    State.setSelectedContactActivity(false);
   };
 
   public static saveMessage = (login: string, savedMessage: Message): void => {
@@ -89,13 +81,13 @@ export default class State {
       }
     }
 
-    if (login === State.selectedContact) {
+    if (login === State.selectedContactLogin) {
       EventEmitter.emit(CustomEventName.MSG_CONTENT_UPDATED);
     }
   };
 
-  public static updateMessageStatus = (updateMsgData: Message): void => {
-    const contactsDataKeys = Object.keys(State.contactsData);
+  public static updateMessage = (updateMsgData: Message, sentMessage = true): void => {
+    const contactsDataKeys = [...State.contactsData.keys()];
 
     for (let i = 0; i < contactsDataKeys.length; i += 1) {
       const login = contactsDataKeys[i]!;
@@ -108,12 +100,7 @@ export default class State {
         if (foundMsg.id === updateMsgData.id) {
           deepMerge(foundMsg, updateMsgData);
 
-          if (foundMsg.status?.isReaded !== updateMsgData.status?.isReaded) {
-            contactData.unreadMessagesCount! += updateMsgData.status?.isReaded ? -1 : 1;
-            EventEmitter.emit(CustomEventName.CONTACTS_UPDATED);
-          }
-
-          if (login === State.selectedContact) {
+          if (login === State.selectedContactLogin && sentMessage) {
             EventEmitter.emit(CustomEventName.MSG_CONTENT_UPDATED);
           }
 
@@ -123,7 +110,14 @@ export default class State {
     }
   };
 
-  public static refillContactMsgHistory = (login: string, messages: Message[]): void => {
+  public static updateMessagesOnRead(readMessages: Message[]): void {
+    readMessages.forEach((message) => {
+      State.updateMessage(message, false);
+    });
+    EventEmitter.emit(CustomEventName.MSG_CONTENT_UPDATED);
+  }
+
+  public static replaceContactMessages = (login: string, messages: Message[]): void => {
     const contactData = State.contactsData.get(login);
 
     if (contactData) {
@@ -139,8 +133,37 @@ export default class State {
       }
     }
 
-    if (login === State.selectedContact) {
+    if (login === State.selectedContactLogin) {
       EventEmitter.emit(CustomEventName.MSG_CONTENT_UPDATED);
+    }
+  };
+
+  public static deleteMessage = (deletedMessage: Message): void => {
+    const contactsDataKeys = [...State.contactsData.keys()];
+
+    for (let i = 0; i < contactsDataKeys.length; i += 1) {
+      const login = contactsDataKeys[i]!;
+      const contactData = State.contactsData.get(login)!;
+      const messages = contactData.messages || [];
+
+      for (let j = 0; j < messages.length; j += 1) {
+        const foundMsg = messages[j]!;
+
+        if (foundMsg.id === deletedMessage.id) {
+          messages.splice(j, 1);
+
+          if (login === State.selectedContactLogin) {
+            EventEmitter.emit(CustomEventName.MSG_CONTENT_UPDATED);
+          }
+
+          if (!foundMsg.status?.isReaded) {
+            contactData.unreadMessagesCount! -= 1;
+            EventEmitter.emit(CustomEventName.CONTACTS_UPDATED);
+          }
+
+          return;
+        }
+      }
     }
   };
 }
