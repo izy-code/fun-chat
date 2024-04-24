051d64c849a30b26303d99674f5adb0c02dc6356
Izy <126877709+izy-code@users.noreply.github.com>
2024-04-24 21:55:57 +0300

    feat: add edit mode to msg sending form

--- fun-chat/src/app/pages/chat/main/dialogue/form/dialogue-form.ts
+++ fun-chat/src/app/pages/chat/main/dialogue/form/dialogue-form.ts
@@ -8,44 +8,74 @@ import CustomEventName from '@/app/custom-events';
 export default class DialogueFormComponent extends BaseComponent<HTMLFormElement> {
   private input: BaseComponent<HTMLInputElement>;
 
-  private button: BaseComponent<HTMLButtonElement>;
+  private sendButton: BaseComponent<HTMLButtonElement>;
+
+  private closeButton: BaseComponent<HTMLButtonElement>;
+
+  private editedMessageId: string | null = null;
 
   constructor() {
     super({ className: 'dialogue__form', tag: 'form' });
 
     this.input = input({ className: 'dialogue__input', type: 'text', placeholder: 'Message', required: true });
-    this.button = ButtonComponent({ className: 'dialogue__button button', textContent: 'Send', buttonType: 'submit' });
-
-    this.button.getNode().disabled = true;
-
-    this.appendChildren([this.input, this.button]);
-
-    this.button.addListener('click', (evt) => {
-      evt.preventDefault();
-      EventEmitter.emit(CustomEventName.SEND_BTN_CLICK, this.input.getNode().value);
-      this.input.getNode().value = '';
-      this.input.getNode().focus();
+    this.sendButton = ButtonComponent({
+      className: 'dialogue__button button',
+      textContent: 'Send',
+      buttonType: 'submit',
+    });
+    this.closeButton = ButtonComponent({
+      className: 'dialogue__close-button button button--cancel',
+      textContent: 'X',
+      buttonType: 'button',
     });
 
+    this.sendButton.getNode().disabled = true;
+
+    this.appendChildren([this.input, this.closeButton, this.sendButton]);
+
+    this.sendButton.addListener('click', (evt) => {
+      evt.preventDefault();
+
+      if (this.editedMessageId) {
+        EventEmitter.emit(CustomEventName.SEND_EDITED_BTN_CLICK, {
+          id: this.editedMessageId,
+          text: this.input.getNode().value,
+        });
+      } else {
+        EventEmitter.emit(CustomEventName.SEND_BTN_CLICK, this.input.getNode().value);
+      }
+
+      this.initForm();
+    });
+
+    this.initForm();
+    this.addListeners();
+  }
+
+  private addListeners = (): void => {
     this.input.addListener('input', () => {
       const message = this.input.getNode().value;
 
-      this.button.getNode().disabled = !message;
+      this.sendButton.getNode().disabled = !message;
     });
-
+    this.closeButton.addListener('click', this.initForm);
     EventEmitter.on(CustomEventName.SELECTED_LOGIN_CHANGED, this.initForm);
+    EventEmitter.on(CustomEventName.EDIT_BTN_CLICK, this.editModeInit);
     window.addEventListener(
       'popstate',
       () => {
-        EventEmitter.on(CustomEventName.SELECTED_LOGIN_CHANGED, this.initForm);
+        EventEmitter.off(CustomEventName.SELECTED_LOGIN_CHANGED, this.initForm);
+        EventEmitter.off(CustomEventName.EDIT_BTN_CLICK, this.editModeInit);
       },
       { once: true },
     );
-  }
+  };
 
   private initForm = (): void => {
     this.input.getNode().value = '';
-    this.button.setAttribute('disabled', '');
+    this.sendButton.setAttribute('disabled', '');
+    this.closeButton.removeClass('dialogue__close-button--shown');
+    this.editedMessageId = null;
 
     if (State.getSelectedContactLogin()) {
       this.input.removeAttribute('disabled');
@@ -53,5 +83,12 @@ export default class DialogueFormComponent extends BaseComponent<HTMLFormElement
     } else {
       this.input.setAttribute('disabled', '');
     }
+  };
+
+  private editModeInit = ({ id, text }: { id: string; text: string }): void => {
+    this.closeButton.addClass('dialogue__close-button--shown');
+    this.editedMessageId = id;
+    this.input.getNode().value = text;
+    this.input.getNode().focus();
   };
 }
